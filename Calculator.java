ca7759e1dcae9cac59161d2b5895bd864b89b925
IliaNovikov <ilja.nowikow2013@yandex.ru>
2023-06-18 18:06:26 +0300

    First version

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+    Predicate<Integer> isNotNull = value -> value != 0;
+
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (isNotNull.test(y)) {
+            return x / y;
+        } else {
+            throw new ArithmeticException("Деление на ноль.");
+        }
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
